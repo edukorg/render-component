5880cbee9f69c84c0640370c2a79134fc3e79f21
Lucas Gomide <lucaslg200@gmail.com>
2016-05-20 14:17:36 -0300

    Force enconding response

--- lib/render/component/helper.rb
+++ lib/render/component/helper.rb
@@ -1,8 +1,10 @@
+# encoding: UTF-8
+
 module Render::Component
   module Helper
 
     def render_component(component, attributes = {})
-      Client.new.obtain_component(component, attributes)
+      Client.new.obtain_component(component, attributes).force_encoding(Encoding::UTF_8)
     end
     module_function :render_component
   end
